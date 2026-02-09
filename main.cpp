ecc97ad01c0c2dea65cf2570bc418824cbd042f5
Jyotirathore12 <jyoti07r@gmail.com>
2026-02-09 20:52:57 +0530

    For loop

--- main.cpp
+++ main.cpp
@@ -6,9 +6,17 @@ int main(){
  int b =3;
  int c = a+b;
  cout<<c;
+ //while loop
  while(a){
   cout<<a<<endl;
   a--;
+ }
+ //for loop 
+ for(int i =0; i<10 ; i++){
+  if(i==3){
+   continue; //skip 3
+  }
+   cout<<i<<" "; //can use endl also to give the space
  }
 return 0;
 }
