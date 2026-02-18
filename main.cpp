03a976fc4bcc9d1fa561a81e29043d95718b15f2
Jyotirathore12 <jyoti07r@gmail.com>
2026-02-18 23:25:40 +0530

    Add unordered_map frequency counter and modern STL usage

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>   
-#include <numeric>     // for accumulate
+#include <algorithm>    // sort, for_each, count_if
+#include <numeric>      // accumulate
+#include <unordered_map> // 🔥 New Feature: Hash Map
+
 using namespace std;
 
+// Function declarations
 void print();
 void displayVector(const vector<int>& v);
 
@@ -16,27 +19,37 @@ int main() {
     int c = a + b;
     cout << "Sum: " << c << endl;
 
+    // while loop
     while (a) {
         cout << a << endl;
         a--;
     }
 
+    // for loop + continue
     for (int i = 0; i < 10; i++) {
         if (i == 3) continue;
         cout << i << " ";
     }
     cout << endl;
 
+    // switch case
     int i;
     cout << "Enter value of i (1 or 2): ";
     cin >> i;
 
     switch (i) {
-        case 1: cout << "Yes" << endl; break;
-        case 2: cout << "No" << endl; break;
-        default: cout << "None" << endl; break;
+        case 1:
+            cout << "Yes" << endl;
+            break;
+        case 2:
+            cout << "No" << endl;
+            break;
+        default:
+            cout << "None" << endl;
+            break;
     }
 
+    // function call
     print();
 
     // STL vector + sorting
@@ -44,7 +57,7 @@ int main() {
     sort(numbers.begin(), numbers.end());
     displayVector(numbers);
 
-    // 🔥 New Feature: Lambda Function
+    // 🔥 Lambda function with for_each
     cout << "Even numbers: ";
     for_each(numbers.begin(), numbers.end(), [](int num) {
         if (num % 2 == 0)
@@ -52,16 +65,28 @@ int main() {
     });
     cout << endl;
 
-    // Using lambda with count_if
+    // Lambda with count_if
     int evenCount = count_if(numbers.begin(), numbers.end(), [](int num) {
         return num % 2 == 0;
     });
-
     cout << "Count of even numbers: " << evenCount << endl;
+
+    // 🔥 New Feature: unordered_map (Frequency Counter)
+    unordered_map<int, int> freq;
+
+    for (int num : numbers) {
+        freq[num]++;
+    }
+
+    cout << "Frequency of elements:" << endl;
+    for (const auto& p : freq) {
+        cout << p.first << " -> " << p.second << endl;
+    }
 
     return 0;
 }
 
+// Function definitions
 void print() {
     cout << "What's in your mind?" << endl;
     cout << "Here's my day in my life" << endl;
